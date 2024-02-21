01ccaa7eb5551125619043ac70c6a5cad317655b
Eric Kobby Mensah <mensaheric743@hotmail.com>
2024-02-21 17:19:15 +0000

    feat: added tap gesture

--- components/Buttons/PrimaryButton.tsx
+++ components/Buttons/PrimaryButton.tsx
@@ -1,39 +1,39 @@
 import { Pressable, StyleSheet, View, Text } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { styles } from './styles';
+import { styles } from "./styles";
 import { ButtonProps } from "../../types";
 
-const PrimaryButton = ({ label, onPress } : ButtonProps) => {
+const PrimaryButton = ({ label, onPress }: ButtonProps) => {
   return (
     <View style={primaryStyle.buttonContainer}>
       <Pressable style={primaryStyle.button} onPress={onPress}>
-      <FontAwesome
-            name="picture-o"
-            size={18}
-            color="#25292e"
-            style={styles.buttonIcon}
-          />
+        <FontAwesome
+          name="picture-o"
+          size={18}
+          color="#25292e"
+          style={styles.buttonIcon}
+        />
         <Text style={primaryStyle.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const primaryStyle = StyleSheet.create({
   buttonContainer: {
     ...styles.buttonContainer,
     borderWidth: 4,
     borderColor: "#ffd33d",
-    borderRadius: 18
+    borderRadius: 18,
   },
   button: {
     ...styles.button,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   buttonLabel: {
     ...styles.buttonLabel,
-    color: "#25292e"
-  }
-})
+    color: "#25292e",
+  },
+});
 
 export default PrimaryButton;
--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,18 +1,45 @@
-import { View, Image, ImageSourcePropType } from 'react-native';
+import { View, ImageSourcePropType } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated/";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 type EmojiStickerProps = {
   imageSize: number;
   stickerSource?: ImageSourcePropType;
 };
 
-export default function EmojiSticker({ imageSize, stickerSource } : EmojiStickerProps) {
+export default function EmojiSticker({
+  imageSize,
+  stickerSource,
+}: EmojiStickerProps) {
+  const scaledImage = useSharedValue(imageSize);
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaledImage.value),
+      height: withSpring(scaledImage.value),
+    };
+  });
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaledImage.value !== imageSize * 2) {
+        scaledImage.value = imageSize * 2;
+      }
+    });
+
   return (
     <View style={{ top: -350 }}>
-      <Image 
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
-  )
+  );
 }
